89bb7aaa7edbeeb0d7a2ac908a14fc7dfc9b5596
Nijat Hamid <nicatorium@gmail.com>
2023-03-20 02:19:33 +0400

    Testing Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import Home from "./pages/Home/Home.jsx";
 import { chakra, useColorModeValue } from "@chakra-ui/react";
 import bgShapeLight from "./assets/img/effect-onlight.png";
 import bgShapeDark from "./assets/img/effect-ondark.png";
-import Layout from "./components/Layout/Layout.jsx";
+// import Layout from "./components/Layout/Layout.jsx";
 import Products from "./pages/Products/Products.jsx";
 import Dashboard from "./pages/Dashboard/Dashboard.jsx";
 import Invoice from "./pages/Invoice/Invoice.jsx";
 import SingleProducts from "./pages/SingleProduct/SingleProducts.jsx";
 import TechStack from "./pages/TechStack/TechStack.jsx";
+import Test from "./pages/Test/Test.jsx";
 
 function App() {
   const bgShape = useColorModeValue(bgShapeLight, bgShapeDark);
@@ -22,7 +23,7 @@ function App() {
       className="App"
     >
       <Router>
-        <Layout>
+        {/* <Layout> */}
           <Routes>
             <Route path="/" exact element={<Home />} />
             <Route path="/products" element={<Products />} />
@@ -30,8 +31,9 @@ function App() {
             <Route path="/invoice" element={<Invoice/>} />
             <Route path="/products/:productID" element={<SingleProducts/>} />
             <Route path="/tech" element={<TechStack/>} />
+            <Route path="/test" element={<Test/>} />
           </Routes>
-        </Layout>
+        {/* </Layout> */}
       </Router>
     </chakra.div>
   );
