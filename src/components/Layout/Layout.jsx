dd19327bd6ce3082b49cee156a8569d7257c73d7
Nijat Hamid <nicatorium@gmail.com>
2023-03-20 01:34:28 +0400

    Reverting some changes

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -7,7 +7,7 @@ function Layout({children}) {
     const [nav,setNav]= useState(false);
     return ( 
         <Flex>
-            {/* <Navbar setNav={()=>setNav(false)} navState={nav}/> */}
+            <Navbar setNav={()=>setNav(false)} navState={nav}/>
             <Box w={{base:"100%",lg:"calc(100% - 100px)"}} position={nav? "fixed":"static"} overflow={nav? "hidden":"auto"} ml={{base:"0",lg:"100px"}}>
                 <Header setNav={()=>setNav(true)} />
                 {children}
