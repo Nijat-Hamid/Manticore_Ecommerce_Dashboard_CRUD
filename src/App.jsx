3bf128a7852d63dea75a5978109d477077512a22
Nijat Hamid <nicatorium@gmail.com>
2023-03-20 02:24:51 +0400

    Reverted all Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
-// import { chakra, useColorModeValue } from "@chakra-ui/react";
-// import bgShapeLight from "./assets/img/effect-onlight.png";
-// import bgShapeDark from "./assets/img/effect-ondark.png";
-// import Layout from "./components/Layout/Layout.jsx";
+import { chakra, useColorModeValue } from "@chakra-ui/react";
+import bgShapeLight from "./assets/img/effect-onlight.png";
+import bgShapeDark from "./assets/img/effect-ondark.png";
+import Layout from "./components/Layout/Layout.jsx";
 import Products from "./pages/Products/Products.jsx";
 import Dashboard from "./pages/Dashboard/Dashboard.jsx";
 import Invoice from "./pages/Invoice/Invoice.jsx";
 import SingleProducts from "./pages/SingleProduct/SingleProducts.jsx";
 import TechStack from "./pages/TechStack/TechStack.jsx";
-import Test from "./pages/Test/Test.jsx";
 
 function App() {
-  // const bgShape = useColorModeValue(bgShapeLight, bgShapeDark);
+  const bgShape = useColorModeValue(bgShapeLight, bgShapeDark);
   return (
-    <div
-      // bgImage={bgShape}
-      // bgRepeat="no-repeat"
-      // bgAttachment="fixed"
-      // minH="100vh"
-      // bgPosition="top left"
+    <chakra.div
+      bgImage={bgShape}
+      bgRepeat="no-repeat"
+      bgAttachment="fixed"
+      minH="100vh"
+      bgPosition="top left"
       className="App"
     >
       <Router>
-        {/* <Layout> */}
+        <Layout>
           <Routes>
             <Route path="/" exact element={<Home />} />
             <Route path="/products" element={<Products />} />
@@ -31,11 +30,10 @@ function App() {
             <Route path="/invoice" element={<Invoice/>} />
             <Route path="/products/:productID" element={<SingleProducts/>} />
             <Route path="/tech" element={<TechStack/>} />
-            <Route path="/test" element={<Test/>} />
           </Routes>
-        {/* </Layout> */}
+        </Layout>
       </Router>
-    </div>
+    </chakra.div>
   );
 }
 
