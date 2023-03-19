c3499362b5bdf2e7b04a6a1361d89cf9230aab34
Nijat Hamid <nicatorium@gmail.com>
2023-03-19 19:39:21 +0400

    Layout misc fixes

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -8,7 +8,7 @@ function Layout({children}) {
     return ( 
         <Flex>
             <Navbar setNav={()=>setNav(false)} navState={nav}/>
-            <Box w={{base:"100%",lg:"calc(100% - 100px)"}} position={nav? "fixed":"static"} overflow={nav? "hidden":"auto"} ml={{base:"0",lg:"100px"}}>
+            <Box w={{base:"100%",lg:"calc(100% - 100px)"}} position={nav? "fixed":"static"}  ml={{base:"0",lg:"100px"}}>
                 <Header setNav={()=>setNav(true)} />
                 {children}
                 <Footer />
