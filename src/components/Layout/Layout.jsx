ff2b2444a45b6afd63124e1d5d783db66bf37218
Nijat Hamid <nicatorium@gmail.com>
2023-03-19 19:42:25 +0400

    Misc

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import { Flex,Box } from "@chakra-ui/react";
 import Navbar from "../Navbar/Navbar.jsx";
-import Footer from "../Footer/Footer.jsx";
+// import Footer from "../Footer/Footer.jsx";
 import Header from "../Header/Header.jsx";
 import { useState } from "react";
 function Layout({children}) {
@@ -8,10 +8,10 @@ function Layout({children}) {
     return ( 
         <Flex>
             <Navbar setNav={()=>setNav(false)} navState={nav}/>
-            <Box w={{base:"100%",lg:"calc(100% - 100px)"}} position={nav? "fixed":"static"}  ml={{base:"0",lg:"100px"}}>
+            <Box w={{base:"100%",lg:"calc(100% - 100px)"}} position={nav? "fixed":"static"} overflow={nav? "hidden":"auto"} ml={{base:"0",lg:"100px"}}>
                 <Header setNav={()=>setNav(true)} />
                 {children}
-                <Footer />
+                {/* <Footer /> */}
             </Box>
         </Flex>
      );
