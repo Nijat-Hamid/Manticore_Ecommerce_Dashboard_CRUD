c27bd159d7a4956e383874ee65f500f8a7cd59ef
Nijat Hamid <nicatorium@gmail.com>
2023-03-20 18:24:55 +0400

    Reacy Lazy & Suspense improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home.jsx";
-import { chakra, useColorModeValue } from "@chakra-ui/react";
+import { chakra, useColorModeValue, Spinner } from "@chakra-ui/react";
 import bgShapeLight from "./assets/img/effect-onlight.png";
 import bgShapeDark from "./assets/img/effect-ondark.png";
-import Layout from "./components/Layout/Layout.jsx";
-import Products from "./pages/Products/Products.jsx";
-import Dashboard from "./pages/Dashboard/Dashboard.jsx";
-import Invoice from "./pages/Invoice/Invoice.jsx";
-import SingleProducts from "./pages/SingleProduct/SingleProducts.jsx";
-import TechStack from "./pages/TechStack/TechStack.jsx";
+// import Home from "./pages/Home/Home.jsx";
+// import Layout from "./components/Layout/Layout.jsx";
+// import Products from "./pages/Products/Products.jsx";
+// import Dashboard from "./pages/Dashboard/Dashboard.jsx";
+// import Invoice from "./pages/Invoice/Invoice.jsx";
+// import SingleProducts from "./pages/SingleProduct/SingleProducts.jsx";
+// import TechStack from "./pages/TechStack/TechStack.jsx";
+import { lazy, Suspense } from "react";
 
+const HomeLazy = lazy(() => import("./pages/Home/Home.jsx"));
+const ProductsLazy = lazy(() => import("./pages/Products/Products.jsx"));
+const DashboardLazy = lazy(() => import("./pages/Dashboard/Dashboard.jsx"));
+const InvoiceLazy = lazy(() => import("./pages/Invoice/Invoice.jsx"));
+const TechStackLazy = lazy(() => import("./pages/TechStack/TechStack.jsx"));
+const SingleProductsLazy = lazy(() =>
+  import("./pages/SingleProduct/SingleProducts.jsx")
+);
+const LayoutLazy = lazy(() => import("./components/Layout/Layout.jsx"));
 function App() {
   const bgShape = useColorModeValue(bgShapeLight, bgShapeDark);
   return (
@@ -22,16 +32,31 @@ function App() {
       className="App"
     >
       <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" exact element={<Home />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/dashboard" element={<Dashboard/>} />
-            <Route path="/invoice" element={<Invoice/>} />
-            <Route path="/products/:productID" element={<SingleProducts/>} />
-            <Route path="/tech" element={<TechStack/>} />
-          </Routes>
-        </Layout>
+        <Suspense
+          fallback={
+            <Spinner
+              thickness="4px"
+              speed="0.65s"
+              emptyColor="gray.200"
+              color="blue.500"
+              size="xl"
+            />
+          }
+        >
+          <LayoutLazy>
+            <Routes>
+              <Route path="/" exact element={<HomeLazy />} />
+              <Route path="/products" element={<ProductsLazy />} />
+              <Route path="/dashboard" element={<DashboardLazy />} />
+              <Route path="/invoice" element={<InvoiceLazy />} />
+              <Route
+                path="/products/:productID"
+                element={<SingleProductsLazy />}
+              />
+              <Route path="/tech" element={<TechStackLazy />} />
+            </Routes>
+          </LayoutLazy>
+        </Suspense>
       </Router>
     </chakra.div>
   );
